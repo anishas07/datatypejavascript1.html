5f4f38bde65af0b25163880b386cded5b8e645d8
anishas07 <Studytea95@gmail.com>
2025-01-03 21:24:47 +1030

    Add files via upload

---
+++ main25.js
@@ -0,0 +1,12 @@
+var a = 2;
+var b = 2;
+var c = 6;
+var d = null ;
+var e;
+document.write("a and b are equal:"+ (a==b)) ;
+document.write("<br>");
+document.write("b and c are equal:" + (b==c)) ;
+document.write("<br>")
+document.write("The value of d is:"+d) ;
+document.write("<br>")
+document.write("The value of e is :" + e);
